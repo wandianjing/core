2fd0d307517e219d8d89ad8dc871c1f914b81562
Vincent Petry <pvince81@owncloud.com>
2017-08-02 12:00:41 +0200

    Use DI and add more tests for storage filter

--- lib/private/Server.php
+++ lib/private/Server.php
@@ -651,6 +651,7 @@ class Server extends ServerContainer implements IServerContainer {
 			}
 			return new NoopLockingProvider();
 		});
+		$this->registerAlias('OCP\Lock\ILockingProvider', 'LockingProvider');
 		$this->registerService('MountManager', function () {
 			return new \OC\Files\Mount\Manager();
 		});
@@ -666,6 +667,7 @@ class Server extends ServerContainer implements IServerContainer {
 				$c->getDatabaseConnection()
 			);
 		});
+		$this->registerAlias('OCP\Files\IMimeTypeLoader', 'MimeTypeLoader');
 		$this->registerService('NotificationManager', function () {
 			return new Manager();
 		});
